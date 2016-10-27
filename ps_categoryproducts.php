34fe0612b15ffb70d15e60646d106910bbf8e972
Maxime Biloé <maxime.biloe@prestashop.com>
2016-10-27 17:39:35 +0200

    // Version updated to v1.0.1

--- ps_categoryproducts.php
+++ ps_categoryproducts.php
@@ -48,7 +48,7 @@ class Ps_Categoryproducts extends Module implements WidgetInterface
     {
         $this->name = 'ps_categoryproducts';
         $this->author = 'PrestaShop';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
 
         $this->bootstrap = true;
         parent::__construct();
